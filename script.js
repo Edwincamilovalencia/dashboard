6c19885805b55410b62d3f9574abd1f010f1e4d4
Edwin Camilo Valencia <78453736+Edwincamilovalencia@users.noreply.github.com>
2024-11-02 11:18:29 -0500

    neew

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Smooth scroll function
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Navbar shrink on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-shrink');
+    } else {
+        navbar.classList.remove('navbar-shrink');
+    }
+});
