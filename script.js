4e4669c5be4c8d82be0209984805271abb6778d8
Edwin Camilo Valencia <78453736+Edwincamilovalencia@users.noreply.github.com>
2024-11-02 19:44:03 -0500

    all

--- script.js
+++ script.js
@@ -15,33 +15,33 @@ window.addEventListener('scroll', function() {
     }
 });
 
-        // Configuración de EmailJS
-        (function() {
-            emailjs.init('eiaRBfy6I2Dacouy4'); // Reemplaza 'tu_user_id' con tu User ID de EmailJS
-        })();
+    // Configuración de EmailJS
+    (function() {
+        emailjs.init('eiaRBfy6I2Dacouy4'); // Reemplaza 'tu_user_id' con tu User ID de EmailJS
+    })();
 
-        // Enviar el formulario
-        document.getElementById('contact-form').addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevenir el envío del formulario de la forma tradicional
+    // Enviar el formulario
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir el envío del formulario de la forma tradicional
 
-            const form = event.target;
-            const name = form.name.value;
-            const email = form.email.value;
-            const message = form.message.value;
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const message = form.message.value;
 
-            // Usar EmailJS para enviar el correo
-            emailjs.send('service_3mxh4gf', 'template_dvrtmwe', {
-                name: name,
-                email: email,
-                message: message
-            })
-            .then(function(response) {
-                alert('Mensaje enviado con éxito!');
-                form.reset(); // Limpiar el formulario después de enviar
-            }, function(error) {
-                alert('Error al enviar el mensaje: ' + JSON.stringify(error));
-            });
+        // Usar EmailJS para enviar el correo
+        emailjs.send('service_3mxh4gf', 'template_dvrtmwe', {
+            name: name,
+            email: email,
+            message: message
+        })
+        .then(function(response) {
+            alert('Mensaje enviado con éxito!');
+            form.reset(); // Limpiar el formulario después de enviar
+        }, function(error) {
+            alert('Error al enviar el mensaje: ' + JSON.stringify(error));
         });
+    });
     
 
     src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
