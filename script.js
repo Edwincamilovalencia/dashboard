781530de9d92498c153cb1486c12e80efa2cfc71
Edwin Camilo Valencia <78453736+Edwincamilovalencia@users.noreply.github.com>
2024-11-02 21:49:36 -0500

    all

--- script.js
+++ script.js
@@ -43,7 +43,5 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     });
 });
 
-
-
     src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
 
