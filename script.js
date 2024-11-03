3d0e40e2320f598c7fea23e45d4348b6e74143c8
Edwin Camilo Valencia <78453736+Edwincamilovalencia@users.noreply.github.com>
2024-11-02 20:03:20 -0500

    ALL

--- script.js
+++ script.js
@@ -15,34 +15,35 @@ window.addEventListener('scroll', function() {
     }
 });
 
-    // Configuración de EmailJS
-    (function() {
-        emailjs.init('eiaRBfy6I2Dacouy4'); // Reemplaza 'tu_user_id' con tu User ID de EmailJS
-    })();
+// Configuración de EmailJS
+(function() {
+    emailjs.init('eiaRBfy6I2Dacouy4'); // Inicializa EmailJS con tu User ID
+})();
 
-    // Enviar el formulario
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevenir el envío del formulario de la forma tradicional
+// Enviar el formulario
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío tradicional
 
-        const form = event.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const message = form.message.value;
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const message = form.message.value;
 
-        // Usar EmailJS para enviar el correo
-        emailjs.send('service_3mxh4gf', 'template_dvrtmwe', {
-            name: name,
-            email: email,
-            message: message
-        })
-        .then(function(response) {
-            alert('Mensaje enviado con éxito!');
-            form.reset(); // Limpiar el formulario después de enviar
-        }, function(error) {
-            alert('Error al enviar el mensaje: ' + JSON.stringify(error));
-        });
+    // Enviar el correo con EmailJS
+    emailjs.send('service_3mxh4gf', 'template_dvrtmwe', {
+        name: name,
+        email: email,
+        message: message
+    })
+    .then(function(response) {
+        alert('Mensaje enviado con éxito!');
+        form.reset(); // Limpia el formulario después de enviar
+    }, function(error) {
+        alert('Error al enviar el mensaje: ' + JSON.stringify(error));
     });
-    
+});
+
+
 
     src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
 
